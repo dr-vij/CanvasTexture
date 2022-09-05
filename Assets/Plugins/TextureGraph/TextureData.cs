1697626a45b0ac89465a1f843608d1a61b7d438a
ViJ <propellerheadvij@gmail.com>
2022-09-05 23:51:28 +0300

    Draw meshes test

--- Assets/Plugins/TextureGraph/TextureData.cs
+++ Assets/Plugins/TextureGraph/TextureData.cs
@@ -16,14 +16,20 @@ namespace ViJApps
 
         public RenderTexture RTex => m_RTex;
 
+        public void Init(RenderTexture rtex)
+        {
+            InitCMD();
+
+            m_Desc = rtex.descriptor;
+            m_RTex = rtex;
+        }
+
         public void Init(int size) => Init(size, size);
 
         public void Init(int width, int height)
         {
-            if (m_Cmd == null)
-                m_Cmd = new CommandBuffer();
-            else
-                m_Cmd.Clear();
+            InitCMD();
+
             if (m_RTex == null || m_RTex.width != width || m_RTex.height != height)
                 ReinitTexture(width, height);
         }
@@ -34,16 +40,58 @@ namespace ViJApps
             m_Cmd.Clear();
         }
 
-        public void SaveToAssets(string assetName)
+        public void ClearWithColor(Color color)
+        {
+            m_Cmd.Clear();
+            m_Cmd.SetRenderTarget(m_RTex);
+            m_Cmd.ClearRenderTarget(RTClearFlags.All, color, 1f, 0);
+        }
+
+        public void DrawLineV1(Vector2 from, Vector2 to, float width, Color color)
+        {
+            var mesh = new Mesh();
+            mesh.SetVertices(new Vector3[] { Vector3.one * 0, Vector3.up * 0.5f, Vector3.one * 0.5f, Vector3.right * 0.5f });
+            mesh.SetIndices(new int[] { 0, 1, 2, 0, 2, 3 }, MeshTopology.Triangles, 0);
+
+            var matrix = GL.GetGPUProjectionMatrix(Matrix4x4.identity, true);
+            m_Cmd.DrawMesh(mesh, matrix, new Material(Shader.Find("Standard")));
+        }
+
+        public void DrawLineV2(Vector2 from, Vector2 to, float width, Color color)
+        {
+            var mesh = new Mesh();
+            mesh.SetVertices(new Vector3[] { Vector3.one * 0, Vector3.up * 0.5f, Vector3.one * 0.5f, Vector3.right * 0.5f });
+            mesh.SetIndices(new int[] { 0, 1, 2, 0, 2, 3 }, MeshTopology.Triangles, 0);
+
+            var matrix = Matrix4x4.identity;
+            m_Cmd.DrawMesh(mesh, matrix, new Material(Shader.Find("Standard")));
+        }
+
+        public Texture2D ToTexture2d()
         {
             var texture2d = new Texture2D(m_RTex.width, m_RTex.height);
             var buffer = RenderTexture.active;
             RenderTexture.active = RTex;
             texture2d.ReadPixels(new Rect(0, 0, m_RTex.width, m_RTex.height), 0, 0);
             texture2d.Apply();
+            RenderTexture.active = buffer;
+            return texture2d;
+        }
+
+        public void SaveToAssets(string assetName)
+        {
+            var texture2d = ToTexture2d();
             var bytes = texture2d.EncodeToPNG();
             System.IO.File.WriteAllBytes($"Assets/" + assetName + ".png", bytes);
-            RenderTexture.active = buffer;
+            AssetDatabase.Refresh();
+        }
+
+        private void InitCMD()
+        {
+            if (m_Cmd == null)
+                m_Cmd = new CommandBuffer();
+            else
+                m_Cmd.Clear();
         }
 
         private void ReinitTexture(int width, int height)
--- Assets/Plugins/TextureGraph/TextureDataExample.cs
+++ Assets/Plugins/TextureGraph/TextureDataExample.cs
@@ -5,11 +5,25 @@ using ViJApps;
 
 public class TextureDataExample : MonoBehaviour
 {
-    private void Awake()
+    [SerializeField] private Renderer m_Renderer1 = default;
+    [SerializeField] private Renderer m_Renderer2 = default;
+
+    private void Start()
     {
         var data = new TextureData();
-        data.Init(128, 128);
+
+        data.Init(1024);
+        data.ClearWithColor(Color.blue);
+        data.DrawLineV1(Vector2.zero, Vector2.zero, 1f, Color.white);
         data.Flush();
-        data.SaveToAssets("Test");
+        m_Renderer1.material.mainTexture = data.ToTexture2d();
+
+        data = new TextureData();
+
+        data.Init(1024);
+        data.ClearWithColor(Color.blue);
+        data.DrawLineV2(Vector2.zero, Vector2.zero, 1f, Color.white);
+        data.Flush();
+        m_Renderer2.material.mainTexture = data.ToTexture2d();
     }
 }
